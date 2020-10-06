d4d8f8de91b8fb7933aab1356124c38c1b643d3b
Andre Sbrocco <andresbrocco@gmail.com>
2020-10-06 14:14:56 +0200

    initial React + TypeScript setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
+const isProd = process.env.NODE_ENV === "production";
+const config = {
+		mode: isProd ? "production" : "development",
+		entry: {
+			index: "./src/index.tsx",
+		},
+		devtool: "inline-source-map",
     module: {
         rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                options: { presets: ["@babel/env"] }
-            },
+						{ 
+								test: /\.tsx?$/,
+                exclude: /node_modules/,
+								use: 'babel-loader'
+						},
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
             }
         ]
     },
-    resolve: { extensions: ['*', '.js', '.jsx'] },
+    resolve: {
+			extensions: ['*', '.ts', '.tsx', '.js', '.jsx']
+		},
     output: {
         path: path.resolve(__dirname, 'dist/'),
         publicPath: '/dist/',
@@ -30,5 +37,28 @@ module.exports = {
         publicPath: 'http://localhost:3000/dist/',
         hotOnly: true
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+    plugins: [
+			new HtmlWebpackPlugin({
+				template: "./public/index.html",
+				filename: "index.html",
+				inject: "body"
+			})
+		]
 };
+
+if (isProd) {
+	config.optimization = {
+		minimizer: [new TerserWebpackPlugin()]
+	};
+} else {
+	config.devServer = {
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		stats: "errors-only",
+		overlay: true
+	};
+};
+
+module.exports = config;
